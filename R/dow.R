e9988d7fd45a54342379a323bbcf869d673ce7f8
Brandan Rosa <brandanrosa22@gmai.com>
2024-04-28 13:32:16 -0500

    Data sets

---
+++ R/dow.R
@@ -0,0 +1,14 @@
+#' DOWSING data set
+#'
+#' A data set of data on whether dowsers can actually divine water
+#'
+#' @format
+#' A data frame with 26 observations and 4 columns:
+#' \describe{
+#'   \item{TRIAL}{Trial number}
+#'   \item{DOWSER}{Assigned number to individual dowsers}
+#'   \item{PIPE}{Actual pipe location}
+#'   \item{GUESS}{Dowsers guess}
+#' }
+#' @source <https://www.amazon.com/Statistics-Engineering-Sciences-William...
+"dow"
